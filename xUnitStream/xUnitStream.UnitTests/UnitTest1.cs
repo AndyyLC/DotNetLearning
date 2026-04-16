d27a61e5f07693c1bfe5318db7ac5a05a5c37ed9
Andyy <andy12312345@hotmail.com>
2026-04-16 17:36:22 -0400

    Can now input Streams into xUnit Tests

---
+++ xUnitStream/xUnitStream.UnitTests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿
+
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+using xUnitStreamLib;
+
+namespace xUnitStream.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void MemoryStream()
+    {
+        using MemoryStream stream = new();
+        StreamWriter writer = new(stream);
+        writer.WriteLine("abc");
+        writer.WriteLine("def");
+        writer.WriteLine("ghi");
+        writer.Flush();
+
+        stream.Seek(0, SeekOrigin.Begin);
+
+        var line = Utils.FindFirstMatchingLine(stream, "f");
+        Assert.Equal("def", line);
+    }
+
+    [Fact]
+    public void FileStream()
+    {
+        var file = new FileInfo("testdata.txt");
+        using var stream = file.OpenRead(); //throw exception if txt file is not found
+        stream.Position = 0;
+
+        var line = Utils.FindFirstMatchingLine(stream, "ou");
+        Assert.Equal("You", line);
+    }
+}
